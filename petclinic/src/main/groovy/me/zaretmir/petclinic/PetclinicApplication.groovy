cd65c66f77c945adcbba049745e26e133019a450
zaretmir <nazaretmiranda@gmail.com>
2020-06-22 10:20:49 +0100

    initial commit

---
+++ petclinic/src/main/groovy/me/zaretmir/petclinic/PetclinicApplication.groovy
@@ -0,0 +1,13 @@
+package me.zaretmir.petclinic
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PetclinicApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PetclinicApplication, args)
+	}
+
+}
---
+++ petclinic/src/test/groovy/me/zaretmir/petclinic/PetclinicApplicationTests.groovy
@@ -0,0 +1,13 @@
+package me.zaretmir.petclinic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
